8cbd06f35caa4a16536381dbbb3297f13fd10c3d
Shrie Nakshathra <nakshathrasrinivas19@gmail.com>
2026-01-20 22:18:01 +0530

    update

--- Online_Movie_Booking_Rest_api/Online_Movie_Booking_Rest_api/src/main/java/com/simplemovie/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.simplemovie.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.simplemovie.entity.Booking;
-
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
